f24a6b94f90db323a5dde5823cb0a1162377f319
Emma Li <emma.m.li.98@gmail.com>
2020-08-25 19:21:10 -0700

    Assignment Complete
    All tests passing (and written)

--- src/dogShimmyDancer.js
+++ src/dogShimmyDancer.js
@@ -1,6 +1,6 @@
 var DogShimmyDancer = function(top, left, timeBetweenSteps) {
   ShimmyDancer.call(this, top, left, timeBetweenSteps);
-  this.$node.prepend('<img src="https://i.ibb.co/MRJZf4g/dogpng.png" width="200" height="200">');
+  this.$node.prepend('<img src="https://i.ibb.co/gDkH560/toby-removebg-preview.png" width="200" height="200">');
 };
 
 DogShimmyDancer.prototype = Object.create(ShimmyDancer.prototype);
